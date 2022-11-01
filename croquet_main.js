603e1a0c55bb7ff268acf1ca393432b92f9b06b2
emartin <eamart34@uw.edu>
2022-11-01 08:31:41 -0700

    testing counting feature:

--- croquet_main.js
+++ croquet_main.js
@@ -1,58 +1,58 @@
 // Hello World site using Croquet
 
 // Q: Still console logging an error here despite importing Croquet in the HTML
-class MyModel extends Croquet.Model {
+// class MyModel extends Croquet.Model {
 
-    init() {
-        console.log("hello world croquet")
-        // document.getElementById("flipStyles").addEventListener("click", toggleStyle); 
-        // this.count = 0;
-        // this.subscribe("counter", "reset", this.resetCounter);
-        // this.future(1000).tick();
-    }
+//     init() {
+//         console.log("hello world croquet")
+//         // document.getElementById("flipStyles").addEventListener("click", toggleStyle); 
+//         // this.count = 0;
+//         // this.subscribe("counter", "reset", this.resetCounter);
+//         // this.future(1000).tick();
+//     }
 
 
-    // resetCounter() {
-    //     // this.count = 0;
-    //     // this.publish("counter", "changed");
-    // }
+//     // resetCounter() {
+//     //     // this.count = 0;
+//     //     // this.publish("counter", "changed");
+//     // }
 
-    // tick() {
-    //     this.count++;
-    //     this.publish("counter", "changed");
-    //     this.future(1000).tick();
-    // }
+//     // tick() {
+//     //     this.count++;
+//     //     this.publish("counter", "changed");
+//     //     this.future(1000).tick();
+//     // }
 
-}
+// }
 
-MyModel.register("MyModel");
+// MyModel.register("MyModel");
 
-class MyView extends Croquet.View {
+// class MyView extends Croquet.View {
 
-    constructor(model) {
-        super(model);
-        this.model = model;
-        // Q: Need to use Croquet Subscribe / Publish events to accomplish this?
-        flipStyles.onclick = event => this.toggleStyle();
-        // this.subscribe("counter", "changed", this.counterChanged);
-        // this.counterChanged();
-    }
+//     constructor(model) {
+//         super(model);
+//         this.model = model;
+//         // Q: Need to use Croquet Subscribe / Publish events to accomplish this?
+//         flipStyles.onclick = event => this.toggleStyle();
+//         // this.subscribe("counter", "changed", this.counterChanged);
+//         // this.counterChanged();
+//     }
 
-    toggleStyle() {
-        let img = document.querySelector("img"); 
-        console.log(img);
-        img.classList.toggle('toggle'); 
-    }
+//     toggleStyle() {
+//         let img = document.querySelector("img"); 
+//         console.log(img);
+//         img.classList.toggle('toggle'); 
+//     }
 
-    // counterReset() {
-    //     this.publish("counter", "reset");
-    // }
+//     // counterReset() {
+//     //     this.publish("counter", "reset");
+//     // }
 
-    // counterChanged() {
-    //     countDisplay.textContent = this.model.count;
-    // }
+//     // counterChanged() {
+//     //     countDisplay.textContent = this.model.count;
+//     // }
 
-}
+// }
 
 // class MyModel extends Croquet.Model {
 
@@ -96,6 +96,54 @@ class MyView extends Croquet.View {
 //     }
 
 // }
+
+// Croquet Tutorial 1
+// Hello World 
+// Croquet Corporation 
+// 2021
+
+class MyModel extends Croquet.Model {
+
+    init() {
+        this.count = 0;
+        this.subscribe("counter", "reset", this.resetCounter);
+        this.future(1000).tick();
+    }
+
+    resetCounter() {
+        this.count = 0;
+        this.publish("counter", "changed");
+    }
+
+    tick() {
+        this.count++;
+        this.publish("counter", "changed");
+        this.future(1000).tick();
+    }
+
+}
+
+MyModel.register("MyModel");
+
+class MyView extends Croquet.View {
+
+    constructor(model) {
+        super(model);
+        this.model = model;
+        countDisplay.onclick = event => this.counterReset();
+        this.subscribe("counter", "changed", this.counterChanged);
+        this.counterChanged();
+    }
+
+    counterReset() {
+        this.publish("counter", "reset");
+    }
+
+    counterChanged() {
+        countDisplay.textContent = this.model.count;
+    }
+
+}
 
 // Q: Users are not able to join a session by using the appId
 Croquet.Session.join({
